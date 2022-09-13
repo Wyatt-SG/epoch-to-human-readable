b3d1830c3d3fc8bf5b9169f6e735cdf746aa1c50
Wyatt Schulte <schultewya@gmail.com>
2022-09-12 22:35:36 -0400

    added eslint and prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
 import convertEpoch from ".";
-import { convertToStandardDateTimeWithTimeZone, convertToStandardDateTime, convertToStandardDate } from "./conversions/conversions";
+import convert from "./convert";
 
-jest.mock("./conversions/conversions", () => ({
-    convertToStandardDateTimeWithTimeZone: jest.fn(),
-    convertToStandardDateTime: jest.fn(),
-    convertToStandardDate: jest.fn()
-}));
+// jest.mock("./convert", () => ({
+//     convertToStandardDateTimeWithTimeZone: jest.fn(),
+//     convertToStandardDateTime: jest.fn(),
+//     convertToStandardDate: jest.fn()
+// }));
 
 describe("calling convertEpoch with incorrect input", () => {
     it("should return undefined epoch isn't a string or number", () => {        
@@ -29,18 +29,18 @@ describe("convertEpoch", () => {
     it("should call convertToStandardDateTimeWithTimeZone when format is not defined", () => {        
         convertEpoch(1662181133);
 
-        expect(convertToStandardDateTimeWithTimeZone).toHaveBeenCalled()
+        expect(convert.toStandardDateTimeWithTimeZone).toHaveBeenCalled()
     });
 
     it("should call convertToStandardDateTime when format = 'SDT'", () => {        
         convertEpoch(1662181133, 'SDT');
 
-        expect(convertToStandardDateTime).toHaveBeenCalled()
+        expect(convert.toStandardDateTime).toHaveBeenCalled()
     });
 
     it("should call convertToStandardDate when format = 'SD'", () => {        
         convertEpoch(1662181133, 'SD');
 
-        expect(convertToStandardDate).toHaveBeenCalled()
+        expect(convert.toStandardDate).toHaveBeenCalled()
     });
 })
